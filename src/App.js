77a03609d29c4863492d0ac2d4233ea2ff214ef0
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-23 16:39:48 +0300

    react icons and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Homepage from "./components/Homepage";  
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
      <Homepage />
+     <Navbar />
     </div>
   );
 }
