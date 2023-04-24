f4683d096e679e4f887166cabc00441beb8f402c
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-24 13:14:18 +0300

    Semi-complete Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Homepage() {
   return (
     <div className='w-full h-screen'>
-        <img className='top-0 w-full object-cover h-screen left-0' src='https://images.pexels.com/photos/10963373/pexels-photo-10963373.jpeg?auto=compress&cs=tinysrgb&w=600' alt='/'/>
+        <img className='top-0 w-full object-cover h-screen left-0' src='https://i.pinimg.com/564x/f4/12/08/f412087ab919dcc0b6c9d193c4d04245.jpg' alt='/'/>
         <div className='bg-black/30 absolute top-0 left-0 w-full h-screen ' />
             <div className='absolute top-0 w-full h-full justify-center text-white flex flex-col'>
                <div className='text-center md:left-[10%] max-w-[1100px] m-auto p-4'>
                 <h1>Nike. Just Do It</h1>
                <p className='font-semibold'>Empower Your Journey with <strong className='text-blue-400 text-3xl'>Nike </strong>. Just Do It</p>
-               <button className='bg-black text-white border-black hover:shadow-xl'>Get Started</button>
+               <button className='bg-black text-white border-black hover:shadow-xl'>Shop Gifts</button>
                </div>
             </div>
       
